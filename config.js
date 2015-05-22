0a2ac65b6703b5432822f1dac31c48cf2182c8f6
Happypea <pabloga265@hotmail.com>
2015-05-22 08:59:45 +0200

    ruta db con puerto

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://212.183.207.4/CRMSINCA',
+	'database': 'mongodb://212.183.207.4:27017/CRMSINCA',
 	'secret': 'norakilledwygum'
 };
